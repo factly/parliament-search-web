2687a2a27ece35ef83f39fad7eff486229bbb401
Monark <monark@factly.in>
2019-07-19 15:02:09 +0530

    Few fixes

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,24 @@ import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import DefaultLayout from '../layouts/index'
+import DefaultLayout from '../layouts/index';
+import Grid from '@material-ui/core/Grid';
+
+const searchWidth = 600;
 
 const useStyles = makeStyles({
   inputroot: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto', 
+    width: searchWidth,
     padding: '2px 8px',
     display: 'flex',
     alignItems: 'center',
-    width: 600,
+    marginBottom: 16
   },
   select: {
-    width: 100,
+    width: searchWidth / 6,
     backgroundColor: ''
   },
   input: {
@@ -29,14 +36,10 @@ const useStyles = makeStyles({
   },
   divider: {
     width: 1,
-    height: 28,
+    height: '28',
     margin: 4,
   },
   header: {
-    position: 'fixed',
-    left: '50%',
-    width: 600,
-    marginLeft: -300,
     marginTop: 60
   }
 });
@@ -49,27 +52,50 @@ export default function Home() {
   return (
     <DefaultLayout>
       <div className={classes.header}>
-      <Paper className={classes.inputroot}>
-        <Select
-          onChange={(event) => {setWhich(event.target.value)}}
-          className={classes.select}
-          value={which}
-        >
-          <MenuItem value={'all'}>All</MenuItem>
-          <MenuItem value={'topics'}>Topics</MenuItem>
-          <MenuItem value={'members'}>Members</MenuItem>
-        </Select>
-        <Divider className={classes.divider} />
-        <InputBase
-          onChange={(event) => {setSearch(event.target.value)}}
-          value={search}
-          className={classes.input}
-          placeholder="Search anything"
-        />
-        <IconButton className={classes.iconButton} aria-label="Search">
-          <SearchIcon />
-        </IconButton>
-      </Paper>
+        <Paper className={classes.inputroot}>
+          <Select
+            disableUnderline={true}
+            onChange={(event) => {setWhich(event.target.value)}}
+            className={classes.select}
+            value={which}
+          >
+            <MenuItem value={'all'}>All</MenuItem>
+            <MenuItem value={'topics'}>Topics</MenuItem>
+            <MenuItem value={'members'}>Members</MenuItem>
+          </Select>
+          <Divider className={classes.divider} />
+          <InputBase
+            onChange={(event) => {setSearch(event.target.value)}}
+            value={search}
+            className={classes.input}
+            placeholder="Search anything"
+          />
+          <IconButton className={classes.iconButton} aria-label="Search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        <div>
+          <Grid container spacing={3}>
+            <Grid item md={2} sm={6}>
+              <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item md={2} sm={6}>
+              <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item md={2} sm={6}>
+              <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item md={2} sm={6}>
+              <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item md={2} sm={6}>
+              <Paper>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item md={2} sm={6}>
+              <Paper>xs=6 sm=3</Paper>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     </DefaultLayout>
   );
