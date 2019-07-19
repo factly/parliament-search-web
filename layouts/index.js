660b9a2c573910b6f4f556086329ab583d33d9cb
Monark <monark@factly.in>
2019-07-19 13:23:44 +0530

    Homepage added

--- layouts/index.js
+++ layouts/index.js
@@ -2,87 +2,38 @@ import React from 'react'
 import Head from "next/head";
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
+import AddLocationIcon from '@material-ui/icons/AddLocation';
+import CategoryIcon from '@material-ui/icons/Category';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-
-const drawerWidth = 240;
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
+  list: {
+    width: 250,
   },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
+  image: {
+    width: 250,
+    height: 'auto'
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  toolbar: theme.mixins.toolbar,
 }));
 
 const DefaultLayout = ({ children }) => {
   const classes = useStyles();
-  const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  function handleDrawerToggle() {
-    setMobileOpen(!mobileOpen);
-  }
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
 
   return (
     <div>
@@ -92,14 +43,13 @@ const DefaultLayout = ({ children }) => {
       </Head>
       <div>
         <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
+        <AppBar position="fixed">
           <Toolbar>
             <IconButton
               color="inherit"
               aria-label="Open drawer"
               edge="start"
-              onClick={handleDrawerToggle}
-              className={classes.menuButton}
+              onClick={() => setMobileOpen(true)}
             >
               <MenuIcon />
             </IconButton>
@@ -108,39 +58,28 @@ const DefaultLayout = ({ children }) => {
             </Typography>
           </Toolbar>
         </AppBar>
-        <nav className={classes.drawer} aria-label="Mailbox folders">
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              variant="temporary"
-              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              ModalProps={{
-                keepMounted: true, // Better open performance on mobile.
-              }}
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              variant="permanent"
-              open
-            >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
+        <Drawer open={mobileOpen} onClose={() => setMobileOpen(false)}>
+          <div
+            className={classes.list}
+            role="presentation"
+            onClose={() => setMobileOpen(false)}
+            onKeyDown={() => setMobileOpen(false)}
+          >
+            <List>
+              <ListItem button>
+                <ListItemIcon><AddLocationIcon /></ListItemIcon>
+                <ListItemText primary={"States"} />
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon><CategoryIcon /></ListItemIcon>
+                <ListItemText primary={"Topics"} />
+              </ListItem>
+            </List>
+          </div>
+        </Drawer>
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          
+          {children}
         </main>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,76 @@
-import React from 'react'
-import Head from "next/head";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import DefaultLayout from '../layouts/index'
 
-function Home() {
-  return(
+const useStyles = makeStyles({
+  inputroot: {
+    padding: '2px 8px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 600,
+  },
+  select: {
+    width: 100,
+    backgroundColor: ''
+  },
+  input: {
+    marginLeft: 16,
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    width: 1,
+    height: 28,
+    margin: 4,
+  },
+  header: {
+    position: 'fixed',
+    left: '50%',
+    width: 600,
+    marginLeft: -300,
+    marginTop: 60
+  }
+});
+
+export default function Home() {
+  const classes = useStyles();
+  const [which, setWhich] = React.useState('all');
+  const [search, setSearch] = React.useState('');
+
+  return (
     <DefaultLayout>
-      <Head>
-        <title>Initial 123</title>
-      </Head>
-      <div>Welcome to Next.js!</div>
-      
+      <div className={classes.header}>
+      <Paper className={classes.inputroot}>
+        <Select
+          onChange={(event) => {setWhich(event.target.value)}}
+          className={classes.select}
+          value={which}
+        >
+          <MenuItem value={'all'}>All</MenuItem>
+          <MenuItem value={'topics'}>Topics</MenuItem>
+          <MenuItem value={'members'}>Members</MenuItem>
+        </Select>
+        <Divider className={classes.divider} />
+        <InputBase
+          onChange={(event) => {setSearch(event.target.value)}}
+          value={search}
+          className={classes.input}
+          placeholder="Search anything"
+        />
+        <IconButton className={classes.iconButton} aria-label="Search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      </div>
     </DefaultLayout>
-  )
+  );
 }
-  
-export default Home;
