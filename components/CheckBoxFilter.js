96749542aaeb449f5bec8408bb631c782ddb165f
Monark <monark@factly.in>
2019-08-14 16:39:35 +0530

    Row prop types added

--- components/CheckBoxFilter.js
+++ components/CheckBoxFilter.js
@@ -8,32 +8,16 @@ import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Collapse from '@material-ui/core/Collapse';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import Input from '@material-ui/core/Input';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const useStyles = makeStyles((theme) => ({
-  paddingZero: {
-    padding: theme.spacing(0),
-  },
-  dataList: {
-    position: 'relative',
-    overflow: 'auto',
-    maxHeight: 250,
-  },
   cardContent: {
-    padding: theme.spacing(1),
     paddingTop: theme.spacing(0),
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: '100%',
   },
   listRow: {
     paddingLeft: theme.spacing(1),
@@ -42,20 +26,6 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1),
     paddingLeft: theme.spacing(2),
     cursor: 'pointer',
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-  },
-  selectedRoot: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    marginLeft: theme.spacing(1),
-  },
-  selected: {
-    marginRight: theme.spacing(0.5),
-    marginBottom: theme.spacing(0.5),
-    borderRadius: theme.spacing(0.5),
   },
   eachFilter: {
     borderRadius: theme.spacing(0),
@@ -68,38 +38,31 @@ const CheckBoxFilter = ({
   const classes = useStyles();
   const [show, setShow] = React.useState(defaultShow);
   const [term, setTerm] = React.useState('');
+  const [options, setOptions] = React.useState(list);
 
-  const filtersList = () => {
-    if (term.length === 0) return list;
+  React.useEffect(() => {
+    if (term.trim().length !== 0) {
+      setOptions(list.filter((value) => value.name.toLowerCase().includes(term.toLowerCase())));
+    } else setOptions(list);
+  }, [term]);
 
-    const newList = [];
-    list.find((value) => {
-      if (value.name.toLowerCase().includes(term.toLowerCase())) newList.push(value);
-    });
-    return newList;
-  };
-
-  const dummyList = filtersList();
   const Row = ({ index, style }) => (
-    <ListItem
+    <FormControlLabel
       style={style}
-      key={dummyList[index].id}
-      className={classes.listRow}
-      dense
-      onClick={() => toogle(dummyList[index].id)}
-    >
-      <ListItemIcon>
+      control={(
         <Checkbox
-          className={classes.paddingZero}
-          checked={selected.indexOf(dummyList[index].id) !== -1}
-          tabIndex={-1}
-          disableRipple
-          inputProps={{ 'aria-labelledby': `checkbox-list-label-${dummyList[index].id}` }}
+          checked={selected.indexOf(options[index].id) !== -1}
+          onChange={() => toogle(options[index].id)}
         />
-      </ListItemIcon>
-      <ListItemText id={`checkbox-list-label-${dummyList[index].id}`} primary={dummyList[index].name} />
-    </ListItem>
+      )}
+      label={options[index].name}
+    />
   );
+
+  Row.propTypes = {
+    index: PropTypes.number.isRequired,
+    style: PropTypes.string.isRequired,
+  };
 
   return (
     <Card className={classes.eachFilter}>
@@ -119,20 +82,24 @@ const CheckBoxFilter = ({
       />
       <Collapse in={show} timeout="auto" unmountOnExit>
         <CardContent className={classes.cardContent}>
-          {
-            search ? (
-              <Input
-                placeholder="Search..."
-                value={term}
-                onChange={(event) => { setTerm(event.target.value); }}
-                className={classes.input}
-              />
-            ) : null
-          }
           <div>
-            <FixedSizeList height={limit * 40} width="100%" itemSize={40} itemCount={filtersList().length}>
-              {Row}
-            </FixedSizeList>
+            <div>
+              {
+              search ? (
+                <Input
+                  fullWidth
+                  placeholder="Search..."
+                  value={term}
+                  onChange={(event) => { setTerm(event.target.value); }}
+                />
+              ) : null
+            }
+            </div>
+            <div>
+              <FixedSizeList height={limit * 40} width="100%" itemSize={40} itemCount={options.length}>
+                {Row}
+              </FixedSizeList>
+            </div>
           </div>
         </CardContent>
       </Collapse>
