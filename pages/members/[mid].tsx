8acf2b9ea89031985afa7a4a9dfbf5bc0ca26fc0
Monark <monark@factly.in>
2019-12-15 13:15:21 +0530

    members page updated

--- pages/members/[mid].tsx
+++ pages/members/[mid].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { useRouter } from 'next/router';
+import { withRouter } from 'next/router';
 import { getMemberById } from '../../store/actions';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -19,15 +19,15 @@ import TableRow from '@material-ui/core/TableRow';
 import Link from 'next/link';
 import {
   typeMemberTerms,
-  typeMemberObject,
-  typeQuestionObject
+  typeMemberData,
+  typeQuestionData
 } from '../../types';
 import { AppState } from '../../store/reducers';
 import moment from 'moment';
 
 interface Props {
-  members: typeMemberObject;
-  questions: typeQuestionObject;
+  member: typeMemberData;
+  questions: typeQuestionData[];
 }
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -52,154 +52,155 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-const MembersPage = ({ members, questions }: Props) => {
-  const mid: number = +useRouter().query.mid;
-  const member = members[mid];
-
+const MembersPage = ({ member, questions }: Props) => {
+ 
   const classes = useStyles();
 
-  if (!member) {
+  if (!member)
     return <div>loading...</div>;
-  } else {
-    return (
-      <div>
-        <Grid>
-          <Card>
-            <CardContent>
-              <Grid container spacing={1}>
-                <Grid item md={2}>
-                  <Avatar
-                    alt="Mp's image"
-                    src="/static/images/mp.jpg"
-                    className={classes.img}
-                  />
+    
+  return (
+    <div>
+      <Grid>
+        <Card>
+          <CardContent>
+            <Grid container spacing={1}>
+              <Grid item md={2}>
+                <Avatar
+                  alt="Mp's image"
+                  src="/static/images/mp.jpg"
+                  className={classes.img}
+                />
+              </Grid>
+              <Grid item md={8}>
+                <Grid>
+                  <Typography variant="h5">{member.name}</Typography>
                 </Grid>
-                <Grid item md={8}>
+                <Grid container direction="row" justify="space-between">
                   <Grid>
-                    <Typography variant="h5">{member.name}</Typography>
+                    <Typography>Gender : {member.gender}</Typography>
+                    {member.birth_place ? (
+                      <Typography>
+                        {' '}
+                        Birthplace : {member.birth_place}
+                      </Typography>
+                    ) : null}
+                    {member.dob ? (
+                      <Typography> Age : {moment.unix(+member.dob / 1000).fromNow(true)}</Typography>
+                    ) : null}
+                    {member.marital_status ? (
+                      <Typography>
+                        Marital Status : {member.marital_status}{' '}
+                      </Typography>
+                    ) : null}
+                    {member.email && member.email.length > 0 ? (
+                      <Typography>
+                        E-mail : {member.email.join(', ')}
+                      </Typography>
+                    ) : null}
+                    {member.phone && member.phone.length > 0 ? (
+                      <Typography>
+                        {' '}
+                        Phone number : {member.phone.join(', ')}{' '}
+                      </Typography>
+                    ) : null}
                   </Grid>
-                  <Grid container direction="row" justify="space-between">
-                    <Grid>
-                      <Typography>Gender : {member.gender}</Typography>
-                      {member.birth_place ? (
-                        <Typography>
-                          {' '}
-                          Birthplace : {member.birth_place}
-                        </Typography>
-                      ) : null}
-                      {member.dob ? (
-                        <Typography> Age : {moment.unix(+member.dob / 1000).fromNow(true)}</Typography>
-                      ) : null}
-                      {member.marital_status ? (
-                        <Typography>
-                          Marital Status : {member.marital_status}{' '}
-                        </Typography>
-                      ) : null}
-                      {member.email && member.email.length > 0 ? (
-                        <Typography>
-                          E-mail : {member.email.join(', ')}
-                        </Typography>
-                      ) : null}
-                      {member.phone && member.phone.length > 0 ? (
-                        <Typography>
-                          {' '}
-                          Phone number : {member.phone.join(', ')}{' '}
-                        </Typography>
-                      ) : null}
-                    </Grid>
-                    <Grid>
-                      {member.education ? (
-                        <Typography>Education : {member.education}</Typography>
-                      ) : null}
-                      {member.profession && member.profession.length > 0 ? (
-                        <Typography>
-                          {' '}
-                          Profession : {member.profession.join(', ')}
-                        </Typography>
-                      ) : null}
-                    </Grid>
+                  <Grid>
+                    {member.education ? (
+                      <Typography>Education : {member.education}</Typography>
+                    ) : null}
+                    {member.profession && member.profession.length > 0 ? (
+                      <Typography>
+                        {' '}
+                        Profession : {member.profession.join(', ')}
+                      </Typography>
+                    ) : null}
                   </Grid>
                 </Grid>
               </Grid>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Card className={classes.marginTopOne}>
-          <CardHeader title="Overview" />
-          <CardContent>
-            <Table className={classes.table} aria-label="MP's terms">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Constituency</TableCell>
-                  <TableCell>Party</TableCell>
-                  <TableCell>House</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {member.terms &&
-                  member.terms.map((term: typeMemberTerms) => (
-                    <TableRow key={term.party.name}>
-                      <TableCell>
-                        <Link
-                          href="/constituencies/[cid]"
-                          as={`/constituencies/${term.constituency.CID}`}
-                        >
-                          <a className={classes.link}>
-                            {term.constituency.name} ({term.constituency.state})
-                          </a>
-                        </Link>
-                      </TableCell>
-                      <TableCell>
-                        <Link
-                          href="/parties/[pid]"
-                          as={`/parties/${term.party.PID}`}
-                        >
-                          <a className={classes.link}>
-                            {term.party.name} ({term.party.abbr})
-                          </a>
-                        </Link>
-                      </TableCell>
-                      <TableCell>
-                        {term.session}, {term.house}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
+            </Grid>
           </CardContent>
         </Card>
-        <Card className={classes.marginTopOne}>
-          <CardHeader
-            title="Popular Questions"
-            action={
-              <Link href={`/search`}>
-                <Button>All Questions</Button>
-              </Link>
-            }
-          />
-          <CardContent>
-            {member.popularQuestionIds &&
-            member.popularQuestionIds.length > 0 ? (
-              member.popularQuestionIds.map((each: number) => (
-                <div key={questions[each].QID} className={classes.marginBottomOne}>
-                  <QuestionBox question={questions[each]} />
-                </div>
-              ))
-            ) : (
-              <p> No questions </p>
-            )}
-          </CardContent>
-        </Card>
-      </div>
-    );
-  }
+      </Grid>
+      <Card className={classes.marginTopOne}>
+        <CardHeader title="Overview" />
+        <CardContent>
+          <Table className={classes.table} aria-label="MP's terms">
+            <TableHead>
+              <TableRow>
+                <TableCell>Constituency</TableCell>
+                <TableCell>Party</TableCell>
+                <TableCell>House</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {member.terms &&
+                member.terms.map((term: typeMemberTerms) => (
+                  <TableRow key={term.party.name}>
+                    <TableCell>
+                      <Link
+                        href="/constituencies/[cid]"
+                        as={`/constituencies/${term.constituency.CID}`}
+                      >
+                        <a className={classes.link}>
+                          {term.constituency.name} ({term.constituency.state})
+                        </a>
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                        href="/parties/[pid]"
+                        as={`/parties/${term.party.PID}`}
+                      >
+                        <a className={classes.link}>
+                          {term.party.name} ({term.party.abbr})
+                        </a>
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      {term.session}, {term.house}
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+      <Card className={classes.marginTopOne}>
+        <CardHeader
+          title="Popular Questions"
+          action={
+            <Link href={`/search`}>
+              <Button>All Questions</Button>
+            </Link>
+          }
+        />
+        <CardContent>
+          {questions ? (
+            questions.map((question: typeQuestionData) => (
+              <div key={question.QID} className={classes.marginBottomOne}>
+                <QuestionBox question={question} />
+              </div>
+            ))
+          ) : (
+            <p> No questions </p>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  ); 
 };
 
 MembersPage.getInitialProps = async ({ store, query }: any) => {
-  await store.dispatch(getMemberById(+query.mid));
+  if(!store.getState().members[+query.mid])
+    await store.dispatch(getMemberById(+query.mid));
 };
-const mapStateToProps = (state: AppState) => ({
-  members: state.members,
-  questions: state.questions
-});
-export default connect(mapStateToProps)(MembersPage);
+const mapStateToProps = (state: AppState, props: any) => {
+  const member = state.members[props.router.query.mid]
+  const popularQuestionIds = member.popularQuestionIds ? member.popularQuestionIds: [] 
+  return {
+    member: member,
+    questions: popularQuestionIds.map(each => state.questions[each])
+  }
+};
+export default withRouter(connect(mapStateToProps)(MembersPage));
